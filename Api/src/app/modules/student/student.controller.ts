4f12451d84ad5c57ed3ed78cbc911583ff8aa178
raidislam <raidislam@gmail.com>
2024-05-23 17:59:06 +0600

    update joi validation

--- Api/src/app/modules/student/student.controller.ts
+++ Api/src/app/modules/student/student.controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from "express";
 import { StudentServices } from "./student.service";
+import studentValidationSchema from "./student.validation";
 //
 const createStudent_C = async function (req: Request, res: Response) {
   try {
     const { student: studentData } = req.body;
 
-    const result = await StudentServices.createStudentIntoDB(studentData);
+    // JOI VALIDATION
+
+    const { error, value } = studentValidationSchema.validate(studentData);
+    const result = await StudentServices.createStudentIntoDB(value);
+
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: "something went wrong",
+        error: error,
+      });
+    }
+
     // send response
     res.status(200).json({
       success: true,
